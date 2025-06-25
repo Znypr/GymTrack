b80d136684576488183aff087146738784f2214b
znypr <sandrooo126@gmail.com>
2025-06-25 23:45:01 +0200

    added dependcy

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,6 +45,7 @@ android {
 dependencies {
     implementation("androidx.room:room-runtime:2.6.1")
     implementation(libs.androidx.navigation.compose)
+    kapt("androidx.room:room-compiler:2.6.1")
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.compose.material3:material3:1.2.1")
     implementation("androidx.room:room-ktx:2.6.1")
