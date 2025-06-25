29feb17b2d35f292b11509e8e97f7bc235382313
znypr <sandrooo126@gmail.com>
2025-06-25 23:38:59 +0200

    update sdk

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,6 +12,7 @@ android {
     defaultConfig {
         applicationId = "com.example.gymtrack"
         minSdk = 24
+        //noinspection OldTargetApi
         targetSdk = 35
         versionCode = 1
         versionName = "1.0"
