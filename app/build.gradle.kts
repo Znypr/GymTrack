6ab75b901f0e41b3353ca05c1ae830dc41399afd
znypr <sandrooo126@gmail.com>
2025-07-03 23:09:44 +0200

    fix

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,7 +15,7 @@ android {
         //noinspection OldTargetApi
         targetSdk = 35
         versionCode = 1
-        versionName = "1.0"
+        versionName = "1.8"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
