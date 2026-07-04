126559e61aeb8d5318ce45229f0ba428a3a60ae7
Znypr <62771035+Znypr@users.noreply.github.com>
2026-07-05 01:51:02 +0200

    feat(data): register canonical entities in Room v9

--- app/src/main/java/com/example/gymtrack/core/data/Database.kt
+++ app/src/main/java/com/example/gymtrack/core/data/Database.kt
@@ -167,13 +167,28 @@ interface SetDao {
 }
 
 @Database(
-    entities = [NoteEntity::class, ExerciseEntity::class, SetEntity::class],
-    version = 8,
+    entities = [
+        NoteEntity::class,
+        ExerciseEntity::class,
+        SetEntity::class,
+        CanonicalCategoryEntity::class,
+        CanonicalExerciseEntity::class,
+        CanonicalExerciseAliasEntity::class,
+        CanonicalWorkoutEntity::class,
+        CanonicalWorkoutExerciseEntity::class,
+        CanonicalWorkoutSetEntity::class,
+    ],
+    version = 9,
 )
 abstract class NoteDatabase : RoomDatabase() {
     abstract fun noteDao(): NoteDao
     abstract fun exerciseDao(): ExerciseDao
     abstract fun setDao(): SetDao
+    abstract fun canonicalCategoryDao(): CanonicalCategoryDao
+    abstract fun canonicalExerciseDao(): CanonicalExerciseDao
+    abstract fun canonicalWorkoutDao(): CanonicalWorkoutDao
+    abstract fun canonicalWorkoutExerciseDao(): CanonicalWorkoutExerciseDao
+    abstract fun canonicalWorkoutSetDao(): CanonicalWorkoutSetDao
 
     companion object {
         @Volatile
